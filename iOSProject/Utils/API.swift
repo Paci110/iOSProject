d0d1616265906209da0b3c249a210a3fd0340cff
Sushi <weingaben@gmail.com>
2022-01-19 04:22:44 +0100

    feat: filtering capabilities for API funcs

--- iOSProject/Utils/API.swift
+++ iOSProject/Utils/API.swift
@@ -46,15 +46,15 @@ func getEvents(start: Date, end: Date, filterFor: [String]? = nil) -> [DateEvent
 }
 
 
-public func getDay(day: Int, month: Int, year: Int) -> [DateEvent] { //TODO: needs value checking
+public func getDay(day: Int, month: Int, year: Int, filterFor: [String]? = nil) -> [DateEvent] { //TODO: needs value checking
     let start = getDate(fromString: "\(day)/\(month)/\(year) 00:00")
     let end = getDate(fromString: "\(day)/\(month)/\(year) 23:59")
     
-    return getEvents(start:start, end:end)
+    return getEvents(start:start, end:end, filterFor: filterFor)
 }
 
 
-public func getWeek(cw: Int, year: Int) -> [[DateEvent]] { //TODO: needs value checking
+public func getWeek(cw: Int, year: Int, filterFor: [String]? = nil) -> [[DateEvent]] { //TODO: needs value checking
     let firstDay = getDate(fromString: "1/1/\(year) 01:00")//FIXME: 00:00 returns 31st december 23:00 in locale 0000 :/
     let offset: Int = 7 - getWeekDay(date: firstDay)
     
@@ -69,21 +69,21 @@ public func getWeek(cw: Int, year: Int) -> [[DateEvent]] { //TODO: needs value c
     for index in 0...6 {
         let date = Date(timeInterval: Double(index) * 24.0 * 60.0 * 60.0, since: start)
         let day = dateToDMY(date: date)
-        let dayEvents = getDay(day: day[0], month:day[1], year:day[2])
+        let dayEvents = getDay(day: day[0], month:day[1], year:day[2], filterFor: filterFor)
         events.append(dayEvents)
     }
     
     return events
 }
 
-public func getMonth(month: Int, year: Int) -> [[DateEvent]] {
+public func getMonth(month: Int, year: Int, filterFor: [String]? = nil) -> [[DateEvent]] {
     let firstDay = getDate(fromString: "1/\(month)/\(year) 01:00")//FIXME: as above
     
     var events: [[DateEvent]] = []
     var currentDay = firstDay
     while(Int(getDate(FromDate: currentDay, Format: "MM"))! == month){
         let day = dateToDMY(date: currentDay)
-        let dayEvents = getDay(day: day[0], month:day[1], year:day[2])
+        let dayEvents = getDay(day: day[0], month:day[1], year:day[2], filterFor: filterFor)
         events.append(dayEvents)
         currentDay = Date(timeInterval: 24.0 * 60.0 * 60.0, since: currentDay)
     }
@@ -91,12 +91,12 @@ public func getMonth(month: Int, year: Int) -> [[DateEvent]] {
     return events
 }
 
-public func getYear(year: Int) -> [[[DateEvent]]] {
+public func getYear(year: Int, filterFor: [String]? = nil) -> [[[DateEvent]]] {
     let firstDay = getDate(fromString: "1/1/\(year)")
     
     var events: [[[DateEvent]]] = []
     for index in 1...12 {
-        let monthEvents = getMonth(month: index, year: year)
+        let monthEvents = getMonth(month: index, year: year, filterFor: filterFor)
         
         events.append(monthEvents)
     }
