f09b0ba50c2db47073df9400d7c3180dd0e1c92d
kowl1307 <kilian.sinke@gmx.de>
2022-01-28 16:58:02 +0100

    added all outlets

--- iOSProject/Views/DateEditViewController.swift
+++ iOSProject/Views/DateEditViewController.swift
@@ -10,4 +10,15 @@ import UIKit
 
 class DateEditViewController: UIViewController {
     
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var fulldaySwitch: UISwitch!
+    @IBOutlet weak var startPicker: UIDatePicker!
+    @IBOutlet weak var endPicker: UIDatePicker!
+    @IBOutlet weak var reminderSwitch: UISwitch!
+    @IBOutlet weak var reminderPicker: UIDatePicker!
+    @IBOutlet weak var notesTextView: UITextView!
+    @IBOutlet weak var urlTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var repeatPicker: UIPickerView!
+    @IBOutlet weak var calendarPicker: UIPickerView!
 }
